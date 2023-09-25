478bd7466131c016be22a4b889463ea079bca801
Hyldem0er <hyldemoer@protonmail.com>
2023-09-25 20:55:55 +0200

    [DONE: Add an exclusion of tv in url
    Signed-off-by: Hyldem0er <hyldemoer@protonmail.com>

--- src/surface_crawl/surface_crawl.py
+++ src/surface_crawl/surface_crawl.py
@@ -12,7 +12,8 @@ def is_not_noise_url(url):
             and "/dir/" not in url and "/pulse/" not in url and "/company/" not in url and "/events/" not in url
             and "/hashtag/" not in url and "/p/" not in url and "/public/" not in url and "/search/" not in url
             and "/filter/" not in url  and "/jobs/" not in url and "/explore/" not in url and "/pages/" not in url
-            and "/reel/" not in url  and "/review/" not in url and "/legacy/" not in url and "/notes/" not in url)
+            and "/reel/" not in url  and "/review/" not in url and "/legacy/" not in url and "/notes/" not in url
+            and "/tv/" not in url)
 
 def extract_profile_url(url):
     """
