76e0dfbd977a1da8252cf4f7d927aa773f102b37
Hyldem0er <138058154+Hyldem0er@users.noreply.github.com>
2023-09-27 16:08:41 +0200

    Added launchers for Windows and Linux

---
+++ launcher-linux.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+is_debian() {
+    if [ -f /etc/debian_version ]; then
+        return 0
+    else
+        return 1
+    fi
+}
+
+sudo apt -y install python3
+sudo apt -y install make
+sudo apt -y install python3.11-venv
+sudo apt -y install python3-pip
+
+if is_debian; then
+    sudo apt -y install libgl1-mesa-glx
+fi
+
+make all
+
+echo "#!/bin/bash
+make run" > launcher.sh
