5641b2c64c2b5a42c546353485c2fb1af74d0949
Hyldem0er <138058154+Hyldem0er@users.noreply.github.com>
2023-09-27 13:48:18 +0200

    Added debian package lib11-mesa-glx that was missing

--- script/launcher-linux.sh
+++ script/launcher-linux.sh
@@ -1,11 +1,28 @@
 #!/bin/bash
 
+# Fonction pour vérifier si le système est Debian
+is_debian() {
+    if [ -f /etc/debian_version ]; then
+        return 0  # Le système est Debian
+    else
+        return 1  # Le système n'est pas Debian
+    fi
+}
+
+# Installation des paquets communs
 sudo apt -y install python3
 sudo apt -y install make
 sudo apt -y install python3.11-venv
 sudo apt -y install python3-pip
+
+# Vérification et installation du paquet spécifique pour Debian
+if is_debian; then
+    sudo apt -y install libgl1-mesa-glx
+fi
+
+# Exécution de la commande make all
 make all
 
+# Création du script de lancement
 echo "#!/bin/bash
-
 make run" > launcher.sh
